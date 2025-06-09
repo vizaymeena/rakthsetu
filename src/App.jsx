fc8667dc46b4d9aa64787da86573d3e5c218a01c
vizaymeena <vizaymeena@gmail.com>
2025-06-09 23:05:53 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import './App.css'
 import Layout from './Components/Layout'
 import Register from './Components/Registration'
+import { Route,Routes } from 'react-router-dom'
 
 function App() {
   
 
   return (
     <>
+    <Routes>
+      {/* Landing page and its relative routes */}
+      <Route path='/' element>
+       
+       <Route path="Donate" element={<Donate/>} />
+        <Route path="Info" element={<Info/>} />
+        <Route path="Help" element={<Help/>}/>
+       
+        <Route path="Register" element={<Register/>} />
+      </Route>
+    </Routes>
+
      <Layout/>
      <Register/>
+    
+    
     </>
   )
 }
