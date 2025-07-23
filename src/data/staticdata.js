f2464eaa4c6c5dec037faf2aa6b9a75cecb05640
vizaymeena <vizaymeena@gmail.com>
2025-07-23 11:41:38 +0530

    renamed a jsx to a js file

---
+++ src/data/staticdata.js
@@ -0,0 +1,60 @@
+// Registration form
+export let stateCityData = [
+  { state: "Madhya Pradesh", 
+    cities: ["Bhopal", "Indore", "Jabalpur", "Gwalior", "Ujjain"] },
+  { state: "Maharashtra", cities: ["Mumbai", "Pune", "Nagpur", "Nashik", "Aurangabad"] },
+  { state: "Uttar Pradesh", cities: ["Lucknow", "Kanpur", "Varanasi", "Agra", "Prayagraj"] },
+  { state: "Rajasthan", cities: ["Jaipur", "Udaipur", "Jodhpur", "Ajmer", "Kota"] },
+  { state: "Tamil Nadu", cities: ["Chennai", "Coimbatore", "Madurai", "Tiruchirappalli", "Salem"] },
+  { state: "Karnataka", cities: ["Bengaluru", "Mysuru", "Hubli", "Mangalore", "Belgaum"] },
+  { state: "Gujarat", cities: ["Ahmedabad", "Surat", "Vadodara", "Rajkot", "Gandhinagar"] },
+  { state: "West Bengal", cities: ["Kolkata", "Asansol", "Siliguri", "Durgapur", "Howrah"] },
+  { state: "Bihar", cities: ["Patna", "Gaya", "Muzaffarpur", "Bhagalpur", "Darbhanga"] },
+  { state: "Punjab", cities: ["Ludhiana", "Amritsar", "Jalandhar", "Patiala", "Bathinda"] }
+];
+
+
+// Homepage
+
+export let bloodCompatibility = {
+  'A+': {
+    receive: ['A+', 'A-', 'O+', 'O-'],
+    donate: ['A+', 'AB+']
+  },
+
+  'O+': {
+    receive: ['O+', 'O-'],
+    donate: ['O+', 'A+', 'B+', 'AB+']
+  },
+
+  'B+': {
+    receive: ['B+', 'B-', 'O+', 'O-'],
+    donate: ['B+', 'AB+']
+  },
+
+  'AB+': {
+    receive: ['A+', 'B+', 'AB+', 'O+', 'A-', 'B-', 'AB-', 'O-'],
+    donate: ['AB+']
+  },
+
+  'A-': {
+    receive: ['A-', 'O-'],
+    donate: ['A+', 'A-', 'AB+', 'AB-']
+  },
+
+  'O-': {
+    receive: ['O-'],
+    donate: ['Anyone']
+  },
+
+  'B-': {
+    receive: ['B-', 'O-'],
+    donate: ['B+', 'B-', 'AB+', 'AB-']
+  },
+
+  'AB-': {
+    receive: ['A-', 'B-', 'AB-', 'O-'],
+    donate: ['AB+', 'AB-']
+  }
+}
+
